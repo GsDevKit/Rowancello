e782ff73a2bfc65dbb841978f19751083199625e
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2025-04-10 15:25:34 -0700

    checkpoint

--- bin/install_rowancello.sh
+++ bin/install_rowancello.sh
@@ -4,7 +4,10 @@ newExtent.solo -r 37x battery_g -e snapshots/$1
 
 ./rowancello.topaz -lq
 
-installProject.stone file:/bosch1/users/dhenrich/_stones/git/Rowancello/rowan/specs/Rowancello.ston --projectsHome=/bosch1/users/dhenrich/_stones/git -D
-
 # run the following using SystemUser
 installProject.stone file:/bosch1/users/dhenrich/_stones/git/Rowancello/rowan/specs/Rowancello-Base.ston --projectsHome=/bosch1/users/dhenrich/_stones/git -D -- -I .topazini_sys
+
+# run the following using DataCurator
+installProject.stone file:/bosch1/users/dhenrich/_stones/git/Rowancello/rowan/specs/Rowancello.ston --projectsHome=/bosch1/users/dhenrich/_stones/git -D
+
+

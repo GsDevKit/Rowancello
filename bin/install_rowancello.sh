4e653d0add32653a40b67cf55aa7764e11ae3848
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2025-04-15 18:34:38 -0700

    convert to using seaside user instead of DataCurator user ... DataCurator has too many privileges ... seaside user is neutral

--- bin/install_rowancello.sh
+++ bin/install_rowancello.sh
@@ -7,7 +7,7 @@ newExtent.solo -r 37x battery_g -e snapshots/extent0.rowancello.dbf
 # run the following using SystemUser
 installProject.stone file:/bosch1/users/dhenrich/_stones/git/Rowancello/rowan/specs/Rowancello-Base.ston --projectsHome=/bosch1/users/dhenrich/_stones/git -D -- -I .topazini_sys
 
-# run the following using DataCurator
-installProject.stone file:/bosch1/users/dhenrich/_stones/git/Rowancello/rowan/specs/Rowancello.ston --projectsHome=/bosch1/users/dhenrich/_stones/git -D
+# run the following using seaside
+installProject.stone file:/bosch1/users/dhenrich/_stones/git/Rowancello/rowan/specs/Rowancello.ston --projectsHome=/bosch1/users/dhenrich/_stones/git -D 
 
 
--- bin/rebuild_stone.sh
+++ bin/rebuild_stone.sh
@@ -5,6 +5,11 @@ newExtent.solo -r 37x battery_g -e product/bin/extent0.rowan3.dbf
 #run the following using SystemUser
 installProject.stone file:/bosch1/users/dhenrich/_stones/37x/g_37x_externals_st/RowanClientServicesV3/rowan/specs/RowanClientServices.ston --projectsHome=/bosch1/users/dhenrich/_stones/37x/g_37x_externals_st -D -- -I .topazini_sys
 
-snapshot.stone snapshots --extension=rowancello.dbf
+#run the following using as DataCurator
+
+./createSeasideUser.topaz -lq
+
+
+snapshot.stone snapshots --extension=rowancello.dbf -- -I .topazini_datacurator
 
 ls -altr snapshots

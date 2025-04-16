bf92577a5bb04e06f13676b104fb072ee403807a
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2025-04-16 14:14:00 -0700

    about to trim ancestorsFor:... logic ... assume that projects aren't loaded ... fingers crossed

--- bin/rebuild_stone.sh
+++ bin/rebuild_stone.sh
@@ -4,9 +4,9 @@ newExtent.solo -r 37x battery_g -e product/bin/extent0.rowan3.dbf
 
 #run the following using SystemUser
 installProject.stone file:/bosch1/users/dhenrich/_stones/37x/g_37x_externals_st/RowanClientServicesV3/rowan/specs/RowanClientServices.ston --projectsHome=/bosch1/users/dhenrich/_stones/37x/g_37x_externals_st -D -- -I .topazini_sys
+installProject.stone file:/bosch1/users/dhenrich/_stones/37x/g_37x_externals_st/RowanV3/rowan/specs/Rowan.ston --alias=RowanV3 --projectsHome=/bosch1/users/dhenrich/_stones/37x/g_37x_externals_st -D -- -I .topazini_sys
 
-#run the following using as DataCurator
-
+#run the following using as DataCurator (done as part of topaz script)
 ./createSeasideUser.topaz -lq
 
 

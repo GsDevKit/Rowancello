9f87bf086b8f44ac499444b58ee1aff02f5af011
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2026-04-16 15:48:53 -0700

    put together a little more documentation for Slices, Clusters and Subclusters; adjust bin/rebuild_stone.sh, so you can use a freshly built and attached extent0.rowan3.dbf (skippig reload of RCS and Rowan)

--- bin/rebuild_stone.sh
+++ bin/rebuild_stone.sh
@@ -1,18 +1,19 @@
 set -x
 
-export ROWAN_PROJECTS_HOME=/bosch1/users/dhenrich/_stones/37x/g_37x_externals_st
+# export ROWAN_PROJECTS_HOME=/bosch1/users/dhenrich/_stones/git
 stone_name=battery_g
 
-newExtent.solo -r 37x $stone_name -e product/bin/extent0.rowan3.dbf
+# SO I CAN USE ATTACHED STONE
+# newExtent.solo -r 37x $stone_name -e product/bin/extent0.rowan3.dbf
 
 #run the following using SystemUser
 ./rowancello_system.topaz -lq 
 
 # RowanClientServices needs to be using latest main375 branch
-installProject.stone file:$ROWAN_PROJECTS_HOME/RowanClientServicesV3/rowan/specs/RowanClientServices.ston --alias=RowanClientServicesV3 --projectsHome=$ROWAN_PROJECTS_HOME -D -- -I .topazini_sys
+#installProject.stone file:$ROWAN_PROJECTS_HOME/RowanClientServicesV3/rowan/specs/RowanClientServices.ston --alias=RowanClientServicesV3 --projectsHome=$ROWAN_PROJECTS_HOME -D -- -I .topazini_sys
 
 # RowanV3 needs to be using latest masterV3.5 branch
-installProject.stone file:$ROWAN_PROJECTS_HOME/RowanV3/rowan/specs/Rowan.ston --alias=RowanV3 --projectsHome=$ROWAN_PROJECTS_HOME -D -- -I .topazini_sys
+#installProject.stone file:$ROWAN_PROJECTS_HOME/RowanV3/rowan/specs/Rowan.ston --alias=RowanV3 --projectsHome=$ROWAN_PROJECTS_HOME -D -- -I .topazini_sys
 
 #run the following using as DataCurator (done as part of topaz script)
 ./createSeasideUser.topaz -lq
